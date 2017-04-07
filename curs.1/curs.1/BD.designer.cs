cae5486f511951ccf6d31df9e76005027503f657
edward877 <edward877@mail.ru>
2017-04-07 19:34:01 +0400

    Revert "fixbd"
    This reverts commit 49b9d0c1443052ececf8a0364d9a567f9fc9763c.

--- curs.1/curs.1/BD.designer.cs
+++ curs.1/curs.1/BD.designer.cs
@@ -25,8 +25,12 @@ namespace curs._1
     {
 		
 		private static MappingSource mappingSource = new AttributeMappingSource();
-
-        public DB(string connection) : base(connection, mappingSource) { }
+		
+		public DB(string connection) : 
+				base(connection, mappingSource)
+		{
+			
+		}
 		
 		public Table<Car> Car
 		{
@@ -67,9 +71,8 @@ namespace curs._1
 				return this.GetTable<Profit_driver>();
 			}
 		}
-        
-        
-        ///// client 
+
+
         public void AddClient(string full_name, string phone_number, string company)
         {
             Client client = new Client();
@@ -104,53 +107,11 @@ namespace curs._1
             Client.DeleteOnSubmit(client);
             SubmitChanges();
         }
-       
+
         public List<Client> ShowClient()
         {
             return Client.Where(c => c.id_client >= 0).ToList();
         }
-
-        /////car 
-        public void AddCar(string number, string brand, decimal carrying_capacity,
-            decimal width, decimal heigth, bool status)
-        {
-            Car car = new Car();
-            car.number = number;
-            car.brand = brand;
-            car.carrying_capacity = carrying_capacity;
-            car.width = width;
-            car.heigth = heigth;
-            car.status = status;
-            Car.InsertOnSubmit(car);
-            SubmitChanges();
-        }
-        
-        public void UpdateCar(int id_car, string number, string brand, decimal carrying_capacity,
-           decimal width, decimal heigth, bool status)
-        {
-            Car car = Car.Where(c => c.id_car == id_car).FirstOrDefault();
-            car.number = number;
-            car.brand = brand;
-            car.carrying_capacity = carrying_capacity;
-            car.width = width;
-            car.heigth = heigth;
-            car.status = status;
-            SubmitChanges();
-        }
-
-        public void DelCar(int id_car)
-        {
-            Car car = Car.Where(c => c.id_car == id_car).FirstOrDefault();
-            Car.DeleteOnSubmit(car);
-            SubmitChanges();
-        }
-
-        public List<Car> ShowCar()
-        {
-            return Car.Where(c => c.id_car >= 0).ToList();
-        }
-
-        /////driver 
     }
 
     [Table(Name="dbo.Car")]
